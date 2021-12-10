cd5b4516bff4dee4b4d3e95efab724660f4dea8b
Dream <94168406+Dream06S@users.noreply.github.com>
2021-12-10 18:11:31 +0100

    foreignexchange test unit

---
+++ Tests/SampleProject.UnitTests/ForeignExchangeTests.cs
@@ -0,0 +1,22 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using SampleProject.Infrastructure.Caching;
+using SampleProject.Infrastructure.Domain.ForeignExchanges;
+using SampleProject.UnitTests.SeedWork;
+
+namespace SampleProject.UnitTests
+{
+    [TestFixture]
+    public class ForeignExchangeTests : TestBase
+    {
+        [Test]
+        public void GetConversionRates_WhenCacheNotAvailable_ShouldReturnTwoValues()
+        {
+            var cacheStore = Substitute.For<ICacheStore>();
+            var foreignExchange = new ForeignExchange(cacheStore);
+            var result = foreignExchange.GetConversionRates();
+
+            Assert.That(result.Count == 2);
+        }
+    }
+}
